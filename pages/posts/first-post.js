f480ee1f091541989d0a3bbe815b562cb9a39d64
crystal <noelani.villanueva@gmail.com>
2022-02-17 20:13:25 -0800

    first post commits

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>first post?</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">

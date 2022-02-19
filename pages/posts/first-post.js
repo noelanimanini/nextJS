7a120fa0f6d076aa8180ae91c3b00eb137cf972b
crystal <noelani.villanueva@gmail.com>
2022-02-19 11:39:06 -0800

    next js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,23 @@
 import Link from "next/link";
 import Head from "next/head";
-import Script from "next/script";
+import Layout from "../../components/layout";
+// import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>first post?</title>
-      </Head>
+      <Layout>
+        <Head>
+          <title>first post?</title>
+        </Head>
 
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>back home</a>
-        </Link>
-      </h2>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>back home</a>
+          </Link>
+        </h2>
+      </Layout>
     </>
   );
 }
